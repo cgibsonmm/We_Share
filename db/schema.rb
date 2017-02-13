1967e4597c66597d98082cfd99e489e53f6fb5ea
Corey Gibson <cgibsonmm@gmail.com>
2017-02-13 15:23:59 -0500

    added forign key to item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213181247) do
+ActiveRecord::Schema.define(version: 20170213201520) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name",               null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170213181247) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
